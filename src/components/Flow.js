e7ad560a087e32a43969d4edcbf618828271817e
tejaswi-spanav <72882843+tejaswi-spanav@users.noreply.github.com>
2021-01-16 15:39:57 +0530

    Update Flow.js

--- src/components/Flow.js
+++ src/components/Flow.js
@@ -4,7 +4,7 @@ import { Container } from 'react-bootstrap';
 function Flow(){
     return(
         <Container style={{padding:'100px'}} >
-            <img src="./flow.png"></img>
+            <img src="https://tejaswi-spanav.github.io/spanav/flow.png"></img>
             {/* <img src={require('\Desktop/flow.png')} /> */}
 
         </Container>
